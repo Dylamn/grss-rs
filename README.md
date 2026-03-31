e6f72dbe1e65377978ba3a7d85b7e766d33d3cef
Dylamn <dylan.vallee@hotmail.com>
2026-03-31 10:47:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# grss-rs
+
+``grss`` is a project that comes from the [rust-cli book](https://rust-cli.github.io/book/index.html).
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+
+    /// The path to the file to read
+    path: PathBuf
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    println!("Pattern: {:?}, path: {:?}", args.pattern, args.path);
+}
