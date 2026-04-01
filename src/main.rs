70dd173e257e709aa7ac4df4b9875cda8ca7216c
Dylamn <dylan.vallee@hotmail.com>
2026-04-01 23:33:17 +0200

    refactor: replace custom error handling with `anyhow` and improve I/O efficiency

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,15 @@
+use anyhow::Context;
 use clap::Parser;
-use std::error::Error;
-use std::fmt;
-use std::fmt::Formatter;
-use std::fs::OpenOptions;
+use std::fs::{File};
 use std::io;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::PathBuf;
-
-#[derive(Debug)]
-enum GrssError {
-    UnexpectedError,
-    FileNotFound,
-    PermissionDenied,
-}
-
-impl fmt::Display for GrssError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match &self {
-            GrssError::UnexpectedError => write!(f, "An unexpected error occurred!"),
-            GrssError::FileNotFound => write!(f, "Could not find the file"),
-            GrssError::PermissionDenied => write!(f, "Cannot open the file, permission denied."),
-        }
-    }
-}
-
-impl Error for GrssError {}
-
-impl From<io::Error> for GrssError {
-    fn from(value: io::Error) -> Self {
-        match value.kind() {
-            io::ErrorKind::NotFound => GrssError::FileNotFound,
-            io::ErrorKind::PermissionDenied => GrssError::PermissionDenied,
-            _ => GrssError::UnexpectedError,
-        }
-    }
-}
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
-
     /// The path to the file to read
     path: PathBuf,
 }
@@ -52,24 +20,25 @@ fn main() {
     }
 }
 
-fn run() -> Result<(), GrssError> {
+fn run() -> anyhow::Result<()> {
     let args = Cli::parse();
 
-    let file = OpenOptions::new()
-        .read(true)
-        .write(false)
-        .open(&args.path)?;
+    let file = File::open(&args.path)
+        .with_context(|| format!("Could not read file `{}`", args.path.display()))?;
 
     let reader = BufReader::new(file);
+    let mut output = BufWriter::new(io::stdout());
 
-    for (nu, read_result) in reader.lines().into_iter().enumerate() {
-        let line = read_result.expect("An error occurred while reading the lines.");
+    for (nu, read_result) in reader.lines().enumerate() {
+        let line = read_result.context("An error occurred while reading the lines.")?;
 
         // Exact matching (case-sensitive)
         if line.contains(&args.pattern) {
-            println!("{}| {}", nu + 1, line);
+            writeln!(output, "{}| {}", nu + 1, line)?;
         }
     }
+
+    output.flush()?;
 
     Ok(())
 }
